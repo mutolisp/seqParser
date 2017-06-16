54d35bb5d6bbc2dccb77a617fd91686d849fc9a4
Lin, Cheng-Tao <mutolisp@gmail.com>
2017-06-16 17:16:40 +0800

    add parameter of outputSeq() 1. add sorting parameter for prunned sequences 2. add range parameter for a given range of prunned sequences

--- R/outputSeq.R
+++ R/outputSeq.R
@@ -2,16 +2,20 @@
 #'
 #' @param fastaFile a fasta file path
 #' @param outputFasta output fasta file path
+#'
 #' @param fprimer forward primer
 #' @param rprimer reverse primer
-#' @param duplinum only select the sequences which duplicate 
+#' @param duplinum only select the sequences which duplicate
 #'        number is greater and equal than `duplinum`
-#' @param fill fill the gap bp with N
+#' @param srange minimum and maximum values of prunned sequence length, ex:
+#'        c(20, 150) means the minimum number is 20 and maximum number is 150
+#' @param sort sort output table by id (1), pruned sequence (2),
+#'        duplicates (3) or pruned sequence length (4)
 #'
 #' @return data.table format sequence set
 
 outputSeq <- function(fastaFile, outputFasta,
-                      fprimer, rprimer, duplinum, fill = TRUE){
+                      fprimer, rprimer, duplinum, srange, sort = 4){
   library(Biostrings)
   #####
   # output sequence
@@ -20,6 +24,10 @@ outputSeq <- function(fastaFile, outputFasta,
   # duplinum: only select duplicate number greater
   #           and equal than n
   ####
+
+  # variables
+  sortMatrix <-  c('id', 'seqPruned', 'duplicates', 'seqPrunedLength')
+
   print("Processing, please wait for a while ...")
   myFasta <- read.myfasta(fastaFile)
   withPrimer <- myFasta[grep(fprimer, seq)][grep(rprimer, seq)]
@@ -39,20 +47,45 @@ outputSeq <- function(fastaFile, outputFasta,
   # append duplicate numbers
   withPrimerNoDesc[, id:= paste(.I, duplicates, sep = "_")]
 
+  # select specific range of sequence length
+  maxPrunedLength <- max(withPrimerNoDesc$seqPrunedLength)
+  minPrunedLength <- min(withPrimerNoDesc$seqPrunedLength)
+
+  reqMin <- as.integer(srange[1])
+  reqMax <- as.integer(srange[2])
+  if ( reqMin < minPrunedLength | reqMax > maxPrunedLength) {
+    sprintf("Invalid range of prunned sequences")
+    break()
+  } else {
+    givenPrimerNoDesc <- withPrimerNoDesc[withPrimerNoDesc[, seqPrunedLength >= reqMin & seqPrunedLength <= reqMax],]
+    # excluded sequences
+    excludedWithPrimerNoDesc <- withPrimerNoDesc[withPrimerNoDesc[, seqPrunedLength < reqMin || seqPrunedLength > reqMax],]
+  }
+  # sort sequences
+  givenPrimerNoDesc <- setkeyv(givenPrimerNoDesc, sortMatrix[sort])
+  excludedWithPrimerNoDesc <- setkeyv(excludedWithPrimerNoDesc, sortMatrix[sort])
+
   if ( file.exists(outputFasta) == TRUE ) {
     origFasta <- paste(outputFasta, 'bak', sep=".")
     file.rename(outputFasta, origFasta)
+    origFasta_d <- paste('deleted_', outputFasta, '.bak', sep="")
+    file.rename(paste('deleted_', outputFasta, sep=""), origFasta_d)
   }
-  if ( fill == TRUE ){
-    maxLength <- max(withPrimerNoDesc[, seqPrunedLength])
-    withPrimerNoDesc[, fn := paste(rep("N", maxLength-seqPrunedLength),
-                                   sep="", collapse=""), by=id]
-    withPrimerNoDesc[, seqPruned := paste(seqPruned, fn, sep="", collapse=""), by=id]
+
+  for ( i in 1:dim(givenPrimerNoDesc)[1] ){
+    seqinr::write.fasta(Biostrings::DNAString(givenPrimerNoDesc[i, seqPruned]),
+                names = givenPrimerNoDesc[i, id], file.out = outputFasta,
+                open = 'a', nbchar = max(givenPrimerNoDesc[, seqPrunedLength]))
   }
-  for ( i in 1:dim(withPrimerNoDesc)[1] ){
-    seqinr::write.fasta(Biostrings::DNAString(withPrimerNoDesc[i, seqPruned]),
-                names = withPrimerNoDesc[i, id], file.out = outputFasta,
-                open = 'a', nbchar = max(withPrimerNoDesc[, seqPrunedLength]))
+  for ( i in 1:dim(excludedWithPrimerNoDesc)[1] ){
+    seqinr::write.fasta(Biostrings::DNAString(excludedWithPrimerNoDesc[i, seqPruned]),
+                        names = excludedWithPrimerNoDesc[i, id], file.out = paste('deleted', outputFasta, sep="_"),
+                        open = 'a', nbchar = max(excludedWithPrimerNoDesc[, seqPrunedLength]))
   }
-  return(withPrimerNoDesc[,.(id, seqPruned, duplicates, seqPrunedLength)])
+
+  target <- givenPrimerNoDesc[,.(id, seqPruned, duplicates, seqPrunedLength)]
+  excluded <- excludedWithPrimerNoDesc[,.(id, seqPruned, duplicates, seqPrunedLength)]
+  resList <- list(target, excluded)
+
+  return(resList)
 }
